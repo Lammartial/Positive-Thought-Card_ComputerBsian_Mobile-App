e44bd370228e8aa64f8503e0b6f542a5c52189f6
Lam Vo <20lam.vkt@vinuni.edu.vn>
2023-01-01 10:46:12 +0700

    update file App.js

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import WelcomeScreen from './app/screens/WelcomeScreen';
-import CardDrawScreen from './app/screens/CardDrawScreen';
+import MainContainer from './app/MainContainer';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <WelcomeScreen />;
+  return (
+    // <WelcomeScreen />
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen options = {{headerShown: false}} name = 'Welcome' component={WelcomeScreen}/>
+            <Stack.Screen options = {{headerShown: false}} name = 'Main' component={MainContainer}/>
+            
+            {/* <Stack.Screen options = {{headerShown: false}} name = 'CardDraw' component={CardDrawScreen}/>
+            <Stack.Screen options = {{headerShown: false}} name = 'Home' component={HomeScreen}/> */}
+
+        </Stack.Navigator>
+    </NavigationContainer>
+
+  ) 
 }
 
 // get window dimensions
