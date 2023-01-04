61efed3a8b62704d79cd4b70c3d50a7b29142090
N3R0162 <20ky.v@vinuni.edu.vn>
2023-01-05 06:35:17 +0700

    VuKy_UserProfile_First

---
+++ constants/Images.js
@@ -0,0 +1,43 @@
+// local imgs
+const Onboarding = require("../assets/imgs/bg.png");
+const Logo = require("../assets/imgs/argon-logo.png");
+const LogoOnboarding = require("../assets/imgs/argon-logo-onboarding.png");
+const ProfileBackground = require("../assets/imgs/background.jpg");
+const RegisterBackground = require("../assets/imgs/register-bg.png");
+const Pro = require("../assets/imgs/getPro-bg.png");
+const ArgonLogo = require("../assets/imgs/argonlogo.png");
+const iOSLogo = require("../assets/imgs/ios.png");
+const androidLogo = require("../assets/imgs/android.png");
+// internet imgs
+
+const ProfilePicture =
+  "https://scontent.fhan2-4.fna.fbcdn.net/v/t1.6435-9/193214346_2846896272306305_4517812867763640559_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=v0uF9RVHNKQAX8CQVAR&_nc_ht=scontent.fhan2-4.fna&oh=00_AfDc7zmsnA_DykUfeB11EALB_Z0oGSITAsYnUj8rl5SNoA&oe=63D62CE7";
+
+const Viewed = [
+  "https://images.unsplash.com/photo-1501601983405-7c7cabaa1581?fit=crop&w=240&q=80",
+  "https://images.unsplash.com/photo-1543747579-795b9c2c3ada?fit=crop&w=240&q=80",
+  "https://images.unsplash.com/photo-1551798507-629020c81463?fit=crop&w=240&q=80",
+  "https://images.unsplash.com/photo-1470225620780-dba8ba36b745?fit=crop&w=240&q=80",
+  "https://images.unsplash.com/photo-1503642551022-c011aafb3c88?fit=crop&w=240&q=80",
+  "https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?fit=crop&w=240&q=80",
+];
+
+const Products = {
+  "View article":
+    "https://images.unsplash.com/photo-1501601983405-7c7cabaa1581?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=840&q=840",
+};
+
+export default {
+  Onboarding,
+  Logo,
+  LogoOnboarding,
+  ProfileBackground,
+  ProfilePicture,
+  RegisterBackground,
+  Viewed,
+  Products,
+  Pro,
+  ArgonLogo,
+  iOSLogo,
+  androidLogo,
+};
