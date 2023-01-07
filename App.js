659760f73f7dc58595eecdc79e24ebc20405c410
Lam Vo <20lam.vkt@vinuni.edu.vn>
2023-01-08 00:31:11 +0700

    update App and package to run normally

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import MainContainer from './app/MainContainer';
-import CardScreen from './app/screens/CardScreen'
+import CardScreen from './app/screens/CardScreen';
+import SettingScreen from './app/screens/SettingScreen';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -16,8 +17,9 @@ export default function App() {
 
             <Stack.Screen options = {{headerShown: false}} name = 'Welcome' component={WelcomeScreen}/>
             <Stack.Screen options = {{headerShown: false}} name = 'Main' component={MainContainer}/>
+            <Stack.Screen options = {{headerShown: false}} name = 'Setting' component={SettingScreen}/>
             
-            {/* <Stack.Screen options = {{headerShown: false}} name = 'CardDraw' component={CardDrawScreen}/>
+            {/* <Stack.Screen options = {{headerShown: false}} name = 'Card' component={CardScreen}/>
             <Stack.Screen options = {{headerShown: false}} name = 'Home' component={HomeScreen}/> */}
 
         </Stack.Navigator>
