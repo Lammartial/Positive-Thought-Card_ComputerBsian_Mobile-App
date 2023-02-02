3a009d93b7107f217533d6b81514d0e4a10c6f5f
N3R0162 <20ky.v@vinuni.edu.vn>
2023-02-02 18:15:02 +0700

    Fix log out button from home screen_ Vu Ky

--- app/screens/AccountScreen.js
+++ app/screens/AccountScreen.js
@@ -27,40 +27,54 @@ const { width, height } = Dimensions.get("screen");
 
 const thumbMeasure = (width - 48 - 32) / 3;
 
-class AccountScreen extends React.Component {
-  useNavigationInClass = () => {
-    const navigation = useNavigation();
-    return navigation;
+function AccountScreen() {
+  const state = {
+    hasGalleyPermission: null,
+    image: null,
   };
 
-  handleSignOut = async () => {
-    const navigation = this.useNavigationInClass();
+  const [sound, setSound] = React.useState();
 
+  async function playSound() {
+    const { sound } = await Audio.Sound.createAsync(
+      require("../assets/sound/button-click.mp3")
+    );
+    setSound(sound);
+
+    await sound.playAsync();
+  }
+
+  React.useEffect(() => {
+    return sound
+      ? () => {
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
+  const navigation = useNavigation();
+
+  const handleSignOut = () => {
     auth
       .signOut()
       .then(() => {
-        // playSound();
+        playSound();
         navigation.replace("Welcome");
       })
       .catch((error) => alert(error.message));
   };
 
-  state = {
-    hasGalleyPermission: null,
-    image: null,
+  const componentDidMount = () => {
+    checkPermission();
   };
 
-  componentDidMount() {
-    this.checkPermission();
-  }
-
-  checkPermission = async () => {
+  const checkPermission = async () => {
     const galleryStatus =
       await ImagePicker.requestMediaLibraryPermissionsAsync();
-    this.setState({ hasGalleryPermission: galleryStatus.status === "granted" });
+    setState({ hasGalleryPermission: galleryStatus.status === "granted" });
   };
 
-  pickImage = async () => {
+  const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
@@ -73,214 +87,206 @@ class AccountScreen extends React.Component {
     }
   };
 
-  render() {
-    const { hasGalleryPermission, image } = this.state;
+  const { hasGalleryPermission, image } = state;
 
-    return (
-      <Block flex style={styles.profile}>
-        <Block flex>
-          <ImageBackground
-            source={Images.ProfileBackground}
-            style={styles.profileContainer}
-            imageStyle={styles.profileBackground}
+  return (
+    <Block flex style={styles.profile}>
+      <Block flex>
+        <ImageBackground
+          source={Images.ProfileBackground}
+          style={styles.profileContainer}
+          imageStyle={styles.profileBackground}
+        >
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+            style={{ width, marginTop: "25%" }}
           >
-            <ScrollView
-              showsVerticalScrollIndicator={false}
-              style={{ width, marginTop: "25%" }}
-            >
-              <Block flex style={styles.profileCard}>
-                <Block middle style={styles.avatarContainer}>
-                  {/* signOut Icon */}
-                  <TouchableOpacity
-                    activeOpacity={0.5}
-                    style={{ position: "absolute", left: 300, bottom: 20 }}
-                    onPress={this.handleSignOut}
-                  >
+            <Block flex style={styles.profileCard}>
+              <Block middle style={styles.avatarContainer}>
+                {/* signOut Icon */}
+                <TouchableOpacity
+                  activeOpacity={0.5}
+                  style={{ position: "absolute", left: 300, bottom: 20 }}
+                  onPress={handleSignOut}
+                >
+                  <Image
+                    source={require("../assets/imgs/signOutIcon.png")}
+                    style={styles.signOutIcon}
+                  />
+                </TouchableOpacity>
+
+                <Text style={styles.signOutText}> Sign out</Text>
+
+                {/* if user log in with guest account */}
+
+                {auth.currentUser.isAnonymous ? (
+                  <Image source={Images.ProfilePicture} style={styles.avatar} />
+                ) : (
+                  <TouchableOpacity activeOpacity={0.5} onPress={pickImage}>
                     <Image
-                      source={require("../assets/imgs/signOutIcon.png")}
-                      style={styles.signOutIcon}
-                    />
-                  </TouchableOpacity>
-
-                  <Text style={styles.signOutText}> Sign out</Text>
-
-                  {/* if user log in with guest account */}
-
-                  {auth.currentUser.isAnonymous ? (
-                    <Image
-                      source={Images.ProfilePicture}
+                      defaultSource={Images.ProfilePicture}
+                      source={{ uri: image }}
                       style={styles.avatar}
                     />
-                  ) : (
-                    <TouchableOpacity
-                      activeOpacity={0.9}
-                      onPress={this.pickImage}
-                    >
-                      <Image
-                        defaultSource={Images.ProfilePicture}
-                        source={{ uri: image }}
-                        style={styles.avatar}
-                      />
 
-                      <Image
-                        source={require("../assets/imgs/camera_icon.png")}
-                        style={styles.camera_icon}
-                      />
-                    </TouchableOpacity>
+                    <Image
+                      source={require("../assets/imgs/camera_icon.png")}
+                      style={styles.camera_icon}
+                    />
+                  </TouchableOpacity>
+                )}
+
+                {hasGalleryPermission === false && (
+                  <Text> No access to Internal Storage</Text>
+                )}
+              </Block>
+
+              {/* username display */}
+
+              <Block flex>
+                <Block middle style={styles.nameInfo}>
+                  {auth.currentUser.isAnonymous && (
+                    <Text bold size={28} color="#32325D">
+                      Guest
+                    </Text>
                   )}
 
-                  {hasGalleryPermission === false && (
-                    <Text> No access to Internal Storage</Text>
+                  {!auth.currentUser.isAnonymous && (
+                    <Text bold size={28} color="#32325D">
+                      {auth.currentUser.displayName}
+                    </Text>
                   )}
+                  <Text size={16} color="#32325D" style={{ marginTop: 10 }}>
+                    Unknown
+                  </Text>
                 </Block>
 
-                {/* username display */}
-
-                <Block flex>
-                  <Block middle style={styles.nameInfo}>
-                    {auth.currentUser.isAnonymous && (
-                      <Text bold size={28} color="#32325D">
-                        Guest
-                      </Text>
-                    )}
-
-                    {!auth.currentUser.isAnonymous && (
-                      <Text bold size={28} color="#32325D">
-                        {auth.currentUser.displayName}
-                      </Text>
-                    )}
-                    <Text size={16} color="#32325D" style={{ marginTop: 10 }}>
-                      Unknown
-                    </Text>
-                  </Block>
-
-                  <Block style={styles.info}>
-                    <Block
-                      middle
-                      row
-                      space="evenly"
-                      style={{ marginTop: 20, paddingBottom: 24 }}
-                    >
-                      <Button
-                        small
-                        style={{ backgroundColor: Theme.COLORS.DEFAULT }}
-                      >
-                        CONNECT
-                      </Button>
-                      <Button
-                        small
-                        style={{ backgroundColor: Theme.COLORS.DEFAULT }}
-                      >
-                        MESSAGE
-                      </Button>
-                    </Block>
-                    <Block row space="between">
-                      <Block middle>
-                        <Text
-                          bold
-                          size={18}
-                          color="#525F7F"
-                          style={{ marginBottom: 4 }}
-                        >
-                          2K
-                        </Text>
-                        <Text size={12} color={Theme.COLORS.TEXT}>
-                          Orders
-                        </Text>
-                      </Block>
-                      <Block middle>
-                        <Text
-                          bold
-                          color="#525F7F"
-                          size={18}
-                          style={{ marginBottom: 4 }}
-                        >
-                          10
-                        </Text>
-                        <Text size={12} color={Theme.COLORS.TEXT}>
-                          Photos
-                        </Text>
-                      </Block>
-                      <Block middle>
-                        <Text
-                          bold
-                          color="#525F7F"
-                          size={18}
-                          style={{ marginBottom: 4 }}
-                        >
-                          89
-                        </Text>
-                        <Text size={12} color={Theme.COLORS.TEXT}>
-                          Comments
-                        </Text>
-                      </Block>
-                    </Block>
-                  </Block>
-
-                  <Block middle style={{ marginTop: 30, marginBottom: 16 }}>
-                    <Block style={styles.divider} />
-                  </Block>
-                  <Block middle>
-                    <Text
-                      size={16}
-                      color="#525F7F"
-                      style={{ textAlign: "center" }}
-                    >
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                    </Text>
-                    <Button
-                      color="transparent"
-                      textStyle={{
-                        color: "#233DD2",
-                        fontWeight: "500",
-                        fontSize: 16,
-                      }}
-                    >
-                      Show more
-                    </Button>
-                  </Block>
-
-                  <Block row space="between">
-                    <Text
-                      bold
-                      size={16}
-                      color="#525F7F"
-                      style={{ marginTop: 12 }}
-                    >
-                      Album
-                    </Text>
+                <Block style={styles.info}>
+                  <Block
+                    middle
+                    row
+                    space="evenly"
+                    style={{ marginTop: 20, paddingBottom: 24 }}
+                  >
                     <Button
                       small
-                      color="transparent"
-                      textStyle={{
-                        color: "#5E72E4",
-                        fontSize: 12,
-                        marginLeft: 24,
-                      }}
+                      style={{ backgroundColor: Theme.COLORS.DEFAULT }}
                     >
-                      View all
+                      CONNECT
+                    </Button>
+                    <Button
+                      small
+                      style={{ backgroundColor: Theme.COLORS.DEFAULT }}
+                    >
+                      MESSAGE
                     </Button>
                   </Block>
-                  <Block style={{ paddingBottom: -HeaderHeight * 2 }}>
-                    <Block row space="between" style={{ flexWrap: "wrap" }}>
-                      {Images.Viewed.map((img, imgIndex) => (
-                        <Image
-                          source={{ uri: img }}
-                          key={`viewed-${img}`}
-                          resizeMode="cover"
-                          style={styles.thumb}
-                        />
-                      ))}
+                  <Block row space="between">
+                    <Block middle>
+                      <Text
+                        bold
+                        size={18}
+                        color="#525F7F"
+                        style={{ marginBottom: 4 }}
+                      >
+                        2K
+                      </Text>
+                      <Text size={12} color={Theme.COLORS.TEXT}>
+                        Orders
+                      </Text>
                     </Block>
+                    <Block middle>
+                      <Text
+                        bold
+                        color="#525F7F"
+                        size={18}
+                        style={{ marginBottom: 4 }}
+                      >
+                        10
+                      </Text>
+                      <Text size={12} color={Theme.COLORS.TEXT}>
+                        Photos
+                      </Text>
+                    </Block>
+                    <Block middle>
+                      <Text
+                        bold
+                        color="#525F7F"
+                        size={18}
+                        style={{ marginBottom: 4 }}
+                      >
+                        89
+                      </Text>
+                      <Text size={12} color={Theme.COLORS.TEXT}>
+                        Comments
+                      </Text>
+                    </Block>
+                  </Block>
+                </Block>
+
+                <Block middle style={{ marginTop: 30, marginBottom: 16 }}>
+                  <Block style={styles.divider} />
+                </Block>
+                <Block middle>
+                  <Text
+                    size={16}
+                    color="#525F7F"
+                    style={{ textAlign: "center" }}
+                  >
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                  </Text>
+                  <Button
+                    color="transparent"
+                    textStyle={{
+                      color: "#233DD2",
+                      fontWeight: "500",
+                      fontSize: 16,
+                    }}
+                  >
+                    Show more
+                  </Button>
+                </Block>
+
+                <Block row space="between">
+                  <Text
+                    bold
+                    size={16}
+                    color="#525F7F"
+                    style={{ marginTop: 12 }}
+                  >
+                    Album
+                  </Text>
+                  <Button
+                    small
+                    color="transparent"
+                    textStyle={{
+                      color: "#5E72E4",
+                      fontSize: 12,
+                      marginLeft: 24,
+                    }}
+                  >
+                    View all
+                  </Button>
+                </Block>
+                <Block style={{ paddingBottom: -HeaderHeight * 2 }}>
+                  <Block row space="between" style={{ flexWrap: "wrap" }}>
+                    {Images.Viewed.map((img, imgIndex) => (
+                      <Image
+                        source={{ uri: img }}
+                        key={`viewed-${img}`}
+                        resizeMode="cover"
+                        style={styles.thumb}
+                      />
+                    ))}
                   </Block>
                 </Block>
               </Block>
-            </ScrollView>
-          </ImageBackground>
-        </Block>
+            </Block>
+          </ScrollView>
+        </ImageBackground>
       </Block>
-    );
-  }
+    </Block>
+  );
 }
 
 const styles = StyleSheet.create({
